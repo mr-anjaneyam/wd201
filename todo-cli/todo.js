5628ef6a211f6621cbbbe1e37730205b481a581f
Sri Anjaneyam <117024325+mr-anjaneyam@users.noreply.github.com>
2023-11-28 21:21:51 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -10,19 +10,19 @@ const todoList = () => {
   const overdue = () => {
     // Write the date check condition here and return the array
     // of overdue items accordingly.
-    return all.filter((todo) => todo.dueDate < today);
+    return all.filter((item) => item.dueDate < new Date().toLocaleDateString("en-CA"));
   };
 
   const dueToday = () => {
     // Write the date check condition here and return the array
     // of todo items that are due today accordingly.
-    return all.filter((todo) => todo.dueDate == today);
+    return all.filter((item) => item.dueDate === new Date().toLocaleDateString("en-CA"));
   };
 
   const dueLater = () => {
     // Write the date check condition here and return the array
     // of todo items that are due later accordingly.
-    return all.filter((todo) => todo.dueDate > today);
+    return all.filter((item) => item.dueDate > new Date().toLocaleDateString("en-CA"));
   };
 
   const toDisplayableList = (list) => {
