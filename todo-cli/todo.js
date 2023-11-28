0ab042e04ff0ad91a0db67d1c51f4a5b79b71f72
Sri Anjaneyam <117024325+mr-anjaneyam@users.noreply.github.com>
2023-11-28 21:38:00 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -24,3 +24,49 @@ const todoList = () => {
     // of todo items that are due later accordingly.
     return all.filter((item) => item.dueDate > new Date().toLocaleDateString("en-CA"));
   };
+
+  const toDisplayableList = (list) => {
+    // Format the To-Do list here, and return the output string
+    // as per the format given above.
+    lst = list
+      .map(
+        (item) =>
+          `${item.completed ? "[x]" : "[ ]"} ${item.title} ${
+            item.dueDate == today ? "" : item.dueDate
+          }`
+      )
+      .join("\n");
+      return lst;
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
+
+const todos = todoList();
+
+const formattedDate = d => {
+  return d.toISOString().split("T")[0]
+}
+
+var dateToday = new Date()
+const today = formattedDate(dateToday)
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+)
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+)
+
+module.exports=todoList;
